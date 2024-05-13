d38aab34441cc284022dbdb2ce33dde04eb8c662
Mencos14 <164411628+Mencos14@users.noreply.github.com>
2024-05-13 12:45:35 -0600

    Add files via upload

---
+++ tarea 3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int numero;
+    unsigned long long factorial = 1;
+
+    cout << "Ingrese un número: ";
+    cin >> numero;
+
+    // Calculando el factorial
+    for(int i = 1; i <= numero; ++i) {
+        factorial *= i;
+    }
+
+    cout << "El factorial de " << numero << " es: " << factorial << endl;
+
+    return 0;
+}
